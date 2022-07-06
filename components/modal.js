f7918035c14a2a42733a6d176034b587014b7f6a
Nevzat Mataraci <91319943+ntmataraci@users.noreply.github.com>
2022-07-06 20:12:22 +0300

    Update modal.js

--- components/modal.js
+++ components/modal.js
@@ -8,7 +8,7 @@ const Modal = ({readingState}) => {
     return(
         <div style={{position:"fixed",top:"0",backgroundColor:"black",opacity:"0.7",width:"100vw",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",zIndex:"99"}}>
  <div style={{maxWidth:"350px",textAlign:"center"}}>
-<p style={{color:"orange",fontWeight:"bolder",fontSize:"1.5rem"}}>It's a duplicate site for study. If you like products please visit : https://foreverqueen.ru/en/</p>
+<p style={{color:"orange",fontWeight:"bolder",fontSize:"1.5rem"}}>It's a duplicate site for study. If you like products please visit : <a href="https://foreverqueen.ru/en/"> foreverqueen.ru/en/</a></p>
 {
 <button  style={{borderRadius:"3rem",backgroundColor:"white",color:"red",padding:"3rem",fontSize:"2rem",cursor:"pointer"}} onClick={()=>readingState()}>ok</button>
 }
